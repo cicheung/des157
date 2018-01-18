c7dec318f2ab2a8925c8668655627d2bffc23f90
Cindy Cheung <cicheung@ucdavis.edu>
2018-01-18 00:31:12 -0800

    studio0
    banner revamped

--- sketch.js
+++ sketch.js
@@ -1,19 +1,42 @@
 console.log("hello");
-var r=0;
-var g=0;
-var b=0;
+var h=0;
+var s=0;
+var l=0;
+var a=100;
 
 function setup() {
   var myCanvas = createCanvas(800,250);
   myCanvas.parent('mySketch');
+  colorMode(HSL);
+  strokeWeight(5);
+
+  // noStroke();
 }
 
 function draw() {
-  r=mouseX;
-  g=mouseY;
-  b=mouseX+mouseY;
-  fill(r,g,b);
+  stroke(h,s,l,a);
 
-  line(mouseX,mouseY,100,80);
-  line(mouseX,mouseY,550,120);
+  if (mouseIsPressed) {
+    h=random(260,300);
+    s=random(20,45);
+    l=random(80,97);
+    line(mouseX,mouseY,pmouseX,pmouseY);
+  } else {
+    h=random(20,55);
+    s=random(80,100);
+    l=random(80,97);
+    line(mouseX,mouseY,pmouseX,pmouseY);
+  }
+  noStroke();
+  fill(70);
+  textSize(12);
+  textAlign(RIGHT,BOTTOM);
+  text("Press SHIFT to start over",width-12,height-12);
+}
+
+function keyPressed() {
+  if (keyCode==SHIFT) {
+    background(255);
+  }
+  return false;
 }
